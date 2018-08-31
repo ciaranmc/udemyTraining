ca8038be1262d5853467438abb6ae6ccb1595533
ciaranmc <ciaranmc@live.co.uk>
2018-08-31 16:03:40 +0100

    Add changes to 02 & 03

---
+++ 02_package/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ciaranmc/udemyTraining/02_package/icomefromalaska"
+	"github.com/ciaranmc/udemyTraining/02_package/stringutil"
+	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
