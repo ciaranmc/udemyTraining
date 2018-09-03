812055498a9d0e349fcd12c2084018fb04c84ed1
ciaranmc <ciaranmc@live.co.uk>
2018-09-03 11:22:46 +0100

    Testing

--- 03_variables/01_shorthand/02/main.go
+++ 03_variables/01_shorthand/02/main.go
@@ -12,8 +12,8 @@ func main() {
 	f := `Do you like my hat?`
 	g := 'M'
 
-	fmt.Printf("%T \n", a)
-	fmt.Printf("%T \n", b)
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
 	fmt.Printf("%T \n", c)
 	fmt.Printf("%T \n", d)
 	fmt.Printf("%T \n", e)
